c5c819f7ead2a15830864112cc68fd0c4e60fed3
joao calado <jcaladomoura@gmail.com>
2022-03-03 23:56:03 -0300

    commit 2

--- monkei/monkei/settings.py
+++ monkei/monkei/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# typically, os.path.join(os.path.dirname(__file__), 'media')
+MEDIA_ROOT = 'mainpage/static/media'
+MEDIA_URL = '/media/'
