ccb76d0387eb2347e25d242070bc9a8f60b0954c
joao calado <jcaladomoura@gmail.com>
2022-03-09 18:38:22 -0300

    commit 3

--- monkei/mainpage/urls.py
+++ monkei/mainpage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    path('', views.home,name='home')
+    path('', views.home,name='home'),
+    path('about/', views.about, name='about')
 ]
 urlpatterns += staticfiles_urlpatterns()
